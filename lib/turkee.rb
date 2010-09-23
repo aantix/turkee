bc2a06217e5781899237bfc944b9421bf82028a2
Jim Jones <jjones@aantix.com>
2010-09-23 09:19:34 -0700

    Started work on turke_form_for helper

--- lib/turkee.rb
+++ lib/turkee.rb
@@ -121,3 +121,41 @@ class TurkeeTask < ActiveRecord::Base
   end
 
 end
+
+
+
+
+
+module TurkeeFormBuilder
+  def mturk_url
+    RAILS_ENV == 'development' ? "https://workersandbox.mturk.com/mturk/externalSubmit" : "https://www.mturk.com/mturk/externalSubmit"
+  end
+
+  # Rails 2.3.8 form_for implementation with the exception of what url it posts to.
+  def turkee_form_for(record_or_name_or_array, *args, &proc)
+    raise ArgumentError, "Missing block" unless block_given?
+
+    options = args.extract_options!
+
+    case record_or_name_or_array
+    when String, Symbol
+      object_name = record_or_name_or_array
+    when Array
+      object = record_or_name_or_array.last
+      object_name = ActionController::RecordIdentifier.singular_class_name(object)
+      apply_form_for_options!(record_or_name_or_array, options)
+      args.unshift object
+    else
+      object = record_or_name_or_array
+      object_name = ActionController::RecordIdentifier.singular_class_name(object)
+      apply_form_for_options!([object], options)
+      args.unshift object
+    end
+
+    # concat(form_tag(options.delete(:url) || {}, options.delete(:html) || {}))
+    concat(form_tag(mturk_url))
+    fields_for(object_name, *(args << options), &proc)
+    concat('</form>'.html_safe)
+  end
+  
+end
