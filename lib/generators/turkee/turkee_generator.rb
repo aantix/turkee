47534605eb02e4c967913827e6b9b8a3c33cb375
Jeremy Clemenson <jclemenson@gmail.com>
2011-04-03 10:19:27 -0700

    minor edit to generator

--- lib/generators/turkee/turkee_generator.rb
+++ lib/generators/turkee/turkee_generator.rb
@@ -7,11 +7,7 @@ class TurkeeGenerator < Rails::Generators::Base
   # Implement the required interface for Rails::Generators::Migration.
   # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
   def self.next_migration_number(dirname)
-    if ActiveRecord::Base.timestamped_migrations
-      Time.now.utc.strftime("%Y%m%d%H%M%S")
-    else
-      "%.3d" % (current_migration_number(dirname) + 1)
-    end
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
   end
 
   source_root File.expand_path("../templates", __FILE__)
