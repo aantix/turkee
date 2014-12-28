6e198259a10ad5e35dee880c16ed37dbab6ffee3
Adam godel <aegodel@gmail.com>
2014-12-28 17:28:24 -0500

    adds more error handling and grabs instance explicitly

--- lib/models/turkee_task.rb
+++ lib/models/turkee_task.rb
@@ -37,16 +37,17 @@ module Turkee
 
               model, param_hash = map_imported_values(assignment, turk.task_type)
               next if model.nil?
-              instance = model.find_by_turkee_task_id(turk.id)
 
               callback_models << model
 
               # If there's a custom approve? method, see if we should approve the submitted assignment
               #  otherwise just approve it by default
-              turk.process_result(assignment, params["upc"], param_hash["retailer_product"]["upc"])
+              turk.process_result(assignment, params["upc"], param_hash["retailer_product"]["upc"]) rescue print("!!" * 100)
 
               TurkeeImportedAssignment.record_imported_assignment(assignment, instance, turk)
             end
+
+            instance = RetailerProductQuery::RetailerProduct.find_by_turkee_task_id(turk.id)
             params.delete("model")
             instance.update_from_params(params, "mechanical_turk") if instance
             turk.set_expired?(callback_models) if !turk.set_complete?(hit, callback_models)
